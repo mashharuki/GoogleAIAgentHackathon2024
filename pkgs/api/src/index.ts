2b59fdbd3d2e63bc89d61aa9d3b58fdc4c9e1487
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-02-02 00:53:46 +0900

    修正

--- pkgs/api/src/index.ts
+++ pkgs/api/src/index.ts
@@ -1,29 +1,16 @@
 import { serve } from "@hono/node-server";
 import { HumanMessage } from "@langchain/core/messages";
-import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { runCdpChatMode } from "./lib/cdpGaiaAgent";
 import { createChatGrogAgent, createCryptTools } from "./lib/chatGrog";
-import { model } from "./lib/langChain";
 import {
+  createAgentTask,
   createGeminiAIAgent,
   createOpenAIAIAgent,
   createTools,
   createVertexAIAIAgent,
 } from "./lib/langGraph";
-import {
-  countTokens,
-  functionCallingChat,
-  functionCallingGenerateContentStream,
-  generateContent,
-  multiPartContent,
-  multiPartContentImageString,
-  multiPartContentVideo,
-  sendChat,
-  streamChat,
-  streamGenerateContent,
-} from "./lib/vertex";
 
 const app = new Hono();
 
@@ -50,167 +37,14 @@ app.get("/health", (c) => {
   });
 });
 
-//streamGenerateContentメソッド
-app.post("/streamGenerateContent", async (c) => {
-  // メソッドを呼び出し
-  const result = await streamGenerateContent();
-  return c.json({
-    result: result,
-  });
-});
-
-// generateContentメソッド
-app.post("/generateContent", async (c) => {
-  const result = await generateContent();
-  return c.json({
-    result: result,
-  });
-});
-
-// streamChatメソッド
-app.post("/streamChat", async (c) => {
-  const result = await streamChat();
-  return c.json({
-    result: result,
-  });
-});
-
-// sendChatメソッド
-app.post("/sendChat", async (c) => {
-  const result = await sendChat();
-  return c.json({
-    result: result,
-  });
-});
-
-// multiPartContentメソッド
-app.post("/multiPartContent", async (c) => {
-  const result = await multiPartContent();
-  return c.json({
-    result: result,
-  });
-});
-
-// multiPartContentImageStringメソッド
-app.post("/multiPartContentImageString", async (c) => {
-  const result = await multiPartContentImageString();
-  return c.json({
-    result: result,
-  });
-});
-
-// multiPartContentVideoメソッド
-app.post("/multiPartContentVideo", async (c) => {
-  const result = await multiPartContentVideo();
-  return c.json({
-    result: result,
-  });
-});
-
-// functionCallingChatメソッド
-app.post("/functionCallingChat", async (c) => {
-  const result = await functionCallingChat();
-  return c.json({
-    result: result,
-  });
-});
-
-// functionCallingGenerateContentStreamメソッド
-app.post("/functionCallingGenerateContentStream", async (c) => {
-  const result = await functionCallingGenerateContentStream();
-  return c.json({
-    result: result,
-  });
-});
-
-// countTokensメソッド
-app.post("/countTokens", async (c) => {
-  const result = await countTokens();
-  return c.json({
-    result: result,
-  });
-});
-
-// langChainメソッド
-app.post("/langChain", async (c) => {
-  const result = await model.invoke([
-    [
-      "human",
-      "What would be a good company name for a company that makes colorful socks?",
-    ],
-  ]);
-
-  return c.json({
-    result: result,
-  });
-});
-
-// OpenAI のLLMを使ったAIAgentのサンプルメソッドを呼び出す
-app.post("/agentOpenAI", async (c) => {
-  const toolNode = createTools();
-  // AI agent用のインスタンスを作成する。
-  const agent = createOpenAIAIAgent(toolNode);
-
-  // Amazon Aurora DSQLについて聞いてみる。
-  const agentNextState = await agent.invoke(
-    { messages: [new HumanMessage("what about Amazon Aurora DSQL?")] },
-    { configurable: { thread_id: "42" } },
-  );
-
-  console.log(
-    agentNextState.messages[agentNextState.messages.length - 1].content,
-  );
-
-  return c.json({
-    result: agentNextState.messages[agentNextState.messages.length - 1].content,
-  });
-});
-
 // Gemini のLLMを使ったAIAgentのサンプルメソッドを呼び出す
 app.post("/agentGemini", async (c) => {
   const toolNode = createTools();
   // GeminiのAI agent用のインスタンスを作成する。
   const agent = createGeminiAIAgent();
 
-  /**
-   * Define the function that determines whether to continue or not
-   * @param param0
-   * @returns
-   */
-  function shouldContinue({ messages }: typeof MessagesAnnotation.State) {
-    const lastMessage = messages[messages.length - 1];
-
-    // If the LLM makes a tool call, then we route to the "tools" node
-    if (lastMessage.additional_kwargs.tool_calls) {
-      return "tools";
-    }
-    // Otherwise, we stop (reply to the user) using the special "__end__" node
-    return "__end__";
-  }
-
-  /**
-   * Define the function that calls the model
-   * @param state
-   * @returns
-   */
-  async function callModel(state: typeof MessagesAnnotation.State) {
-    // AIに推論させる
-    const response = await agent.invoke(state.messages);
-
-    // We return a list, because this will get added to the existing list
-    return { messages: [response] };
-  }
-
   // ワークフローを構築する。
-  const workflow = new StateGraph(MessagesAnnotation)
-    .addNode("agent", callModel)
-    .addEdge("__start__", "agent") // __start__ is a special name for the entrypoint
-    .addNode("tools", toolNode)
-    .addEdge("tools", "agent")
-    .addConditionalEdges("agent", shouldContinue);
-
-  // Finally, we compile it into a LangChain Runnable.
-  const app = workflow.compile();
+  const app = await createAgentTask(agent, toolNode);
 
   // Use the agent
   const finalState = await app.invoke({
@@ -241,60 +75,8 @@ app.post("/agentVertexAI", async (c) => {
   // GeminiのAI agent用のインスタンスを作成する。
   const agent = createVertexAIAIAgent();
 
-  /**
-   * Define the function that determines whether to continue or not
-   * @param param0
-   * @returns
-   */
-  function shouldContinue({ messages }: typeof MessagesAnnotation.State) {
-    const lastMessage = messages[messages.length - 1];
-
-    // If the LLM makes a tool call, then we route to the "tools" node
-    if (lastMessage.additional_kwargs.tool_calls) {
-      return "tools";
-    }
-    // Otherwise, we stop (reply to the user) using the special "__end__" node
-    return "__end__";
-  }
-
-  /**
-   * Define the function that calls the model
-   * @param state
-   * @returns
-   */
-  async function callModel(state: typeof MessagesAnnotation.State) {
-    // AIに推論させる
-    const response = await agent.generateContent({
-      contents: [
-        {
-          role: "model",
-          parts: [
-            {
-              text: `${state.messages[state.messages.length - 1].content.toString()}`,
-            },
-          ],
-        },
-      ],
-    });
-
-    // Extract the first candidate's content
-    const content = response.response.candidates?.[0].content;
-    // Create a HumanMessage object
-    const message = new HumanMessage(content?.parts[0].text as string);
-    // console.log("message:", message)
-    return { messages: [message] };
-  }
-
   // ワークフローを構築する。
-  const workflow = new StateGraph(MessagesAnnotation)
-    .addNode("agent", callModel)
-    .addEdge("__start__", "agent") // __start__ is a special name for the entrypoint
-    .addNode("tools", toolNode)
-    .addEdge("tools", "agent")
-    .addConditionalEdges("agent", shouldContinue);
-
-  // Finally, we compile it into a LangChain Runnable.
-  const app = workflow.compile();
+  const app = await createAgentTask(agent, toolNode);
 
   // Use the agent
   const finalState = await app.invoke({
--- pkgs/api/src/lib/langGraph.ts
+++ pkgs/api/src/lib/langGraph.ts
@@ -1,8 +1,17 @@
-import { VertexAI } from "@google-cloud/vertexai";
-import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+import {
+  type GenerativeModel,
+  HarmBlockThreshold,
+  HarmCategory,
+  VertexAI,
+} from "@google-cloud/vertexai";
 import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
+import { HumanMessage } from "@langchain/core/messages";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import { MemorySaver } from "@langchain/langgraph";
+import {
+  MemorySaver,
+  MessagesAnnotation,
+  StateGraph,
+} from "@langchain/langgraph";
 import { ToolNode, createReactAgent } from "@langchain/langgraph/prebuilt";
 import { ChatOpenAI } from "@langchain/openai";
 import * as dotenv from "dotenv";
@@ -99,4 +108,71 @@ export const createVertexAIAIAgent = () => {
   });
 
   return agent;
+};
+
+/**
+ * AI Agentに実行させるワークフローとタスクを定義するメソッド
+ * @parma AI Agent instance
+ * @param toolNode 外部ツール
+ */
+export const createAgentTask = async (
+  agent: GenerativeModel | ChatGoogleGenerativeAI | VertexAI,
+  toolNode: ToolNode,
+) => {
+  /**
+   * Define the function that determines whether to continue or not
+   * @param param0
+   * @returns
+   */
+  function shouldContinue({ messages }: typeof MessagesAnnotation.State) {
+    const lastMessage = messages[messages.length - 1];
+
+    // If the LLM makes a tool call, then we route to the "tools" node
+    if (lastMessage.additional_kwargs.tool_calls) {
+      return "tools";
+    }
+    // Otherwise, we stop (reply to the user) using the special "__end__" node
+    return "__end__";
+  }
+
+  /**
+   * Define the function that calls the model
+   * @param state
+   * @returns
+   */
+  async function callModel(state: typeof MessagesAnnotation.State) {
+    // AIに推論させる
+    const response = await (agent as GenerativeModel).generateContent({
+      contents: [
+        {
+          role: "model",
+          parts: [
+            {
+              text: `${state.messages[state.messages.length - 1].content.toString()}`,
+            },
+          ],
+        },
+      ],
+    });
+
+    // Extract the first candidate's content
+    const content = response.response.candidates?.[0].content;
+    // Create a HumanMessage object
+    const message = new HumanMessage(content?.parts[0].text as string);
+    // console.log("message:", message)
+    return { messages: [message] };
+  }
+
+  // ワークフローを構築する。
+  const workflow = new StateGraph(MessagesAnnotation)
+    .addNode("agent", callModel)
+    .addEdge("__start__", "agent") // __start__ is a special name for the entrypoint
+    .addNode("tools", toolNode)
+    .addEdge("tools", "agent")
+    .addConditionalEdges("agent", shouldContinue);
+
+  // Finally, we compile it into a LangChain Runnable.
+  const app = workflow.compile();
+
+  return app;
 };
